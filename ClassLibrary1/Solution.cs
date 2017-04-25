64cb3d4f6f97a102fc79b9f331c82d7ca619a2bc
Y2PM <joesamban@yahoo.co.uk>
2017-04-25 17:10:11 +0100

    Initial

---
+++ ClassLibrary1/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Solution
+    {
+        int Length = new int();
+
+        public Solution(int length)
+        {
+            Length = length;
+        }
+
+        public int solutionMethod(string S)//Symmetry finding method.
+        {
+            char[] characterArray = new char[Length];
+            char[] characterArrayReversed = new char[Length];
+            characterArray = S.ToCharArray();
+            int numberOfWays = Length;
+
+            //Create a reversed version:
+            Array.Reverse(characterArray);
+            characterArrayReversed = characterArray;
+            Array.Reverse(characterArray);
+            //
+
+            if (characterArray.Length < 2)
+            {
+                return numberOfWays;
+            }
+
+            //Check for symmetry
+
+            //If even number of elements:
+            if (Length % 2 == 0)
+            {
+                foreach (var item in characterArray)//Start comparing the elements starting with the biggest segment then going to the smallest. 
+                {
+                    
+                }
+            }
+
+            //If odd number of elements:
+            if (Length % 2 == 1)
+            {
+                for (int i = 0; i < length; i++)
+                {
+
+                }
+            }
+
+            return numberOfWays % 1000000007; //Here should be number of segments victor can buy modulo 1000000007.
+        }
+
+
+    }
+}
---
+++ WinterLights.Project/Program.cs
@@ -0,0 +1,50 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterLights.Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "02002";
+            int answer;
+            Solution solutionObject = new Solution(200000);
+
+            answer = solutionObject.solutionMethod(S);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+            /*
+            //Testing reverse method:
+            char[] characterArray = new char[10];
+            string numbers = "0123456789";
+
+            characterArray = numbers.ToCharArray();
+            Console.WriteLine("Not reversed: ");
+            foreach (var item in characterArray)
+            {
+                Console.Write(item);
+            }
+            
+            Array.Reverse(characterArray);
+            Console.WriteLine();
+            Console.WriteLine("Reversed: ");
+            foreach (var item in characterArray)
+            {
+                Console.Write(item);
+            }
+
+            Console.ReadLine();
+            */
+
+            //Console.WriteLine(4 % 2 + " " + 5 % 2);
+            //Console.ReadLine();
+
+        }
+    }
+}
