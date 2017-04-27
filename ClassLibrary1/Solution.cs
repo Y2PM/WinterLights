b1fa6672689353549312d8e62610814486d5d7c1
Y2PM <joesamban@yahoo.co.uk>
2017-04-27 09:56:19 +0100

    made symmetry check method

--- ClassLibrary1/Solution.cs
+++ ClassLibrary1/Solution.cs
@@ -15,7 +15,28 @@ namespace ClassLibrary1
             Length = length;
         }
 
-        public int solutionMethod(string S)//Symmetry finding method. 
+        //Even check symmetry method:
+        private static bool SymmetryCheck(char[] characterArray, int Length)//For even number of elements only.
+        {
+            if (characterArray.Length % 2 != 0)//if not even number of elements.
+            {
+                return false;
+            }
+
+            int EndIndex = Length;
+            for (int i = 0; i < Length / 2; i++)
+            {
+                if (i!=EndIndex)
+                {
+                    return false;
+                }
+                EndIndex--;
+            }
+
+            return true;
+        }
+
+        public int solutionMethod(string S)//Symmetry finding method.
         {
             char[] characterArray = new char[Length];
             char[] characterArrayReversed = new char[Length];
@@ -33,24 +54,23 @@ namespace ClassLibrary1
                 return numberOfWays;
             }
 
-            //Check for symmetry
+
 
             //If even number of elements:
             if (Length % 2 == 0)
             {
-                foreach (var item in characterArray)//Start comparing the elements starting with the biggest segment then going to the smallest. 
+
+
+                if (SymmetryCheck(characterArray, Length))
                 {
-                    
-                }
+                    numberOfWays++;
+                }                
             }
 
             //If odd number of elements:
             if (Length % 2 == 1)
             {
-                for (int i = 0; i < length; i++)
-                {
-
-                }
+         
             }
 
             return numberOfWays % 1000000007; //Here should be number of segments victor can buy modulo 1000000007.
